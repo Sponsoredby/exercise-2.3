34c04c42d2d4f1f58430b4a0d37bb0ba77dd938d
Sponsoredby <74147554+Sponsoredby@users.noreply.github.com>
2020-11-11 11:42:54 +0000

    Add files via upload

---
+++ exercise 2.3.py
@@ -0,0 +1,8 @@
+# This first line is provided for you
+
+hrs = input("Enter Hours:") #enter hours
+rate = input ("Enter rate") #entrate 
+
+
+pay = float(hrs) * float(rate) #create the variable pay while converting str to interger
+print ("Pay:", pay) # print result with text
